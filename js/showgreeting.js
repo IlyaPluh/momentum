9e92139842cc52723ee3f5b8340650d70d1e9c99
IlyaPluh <ilyaandriyanov@mail.ru>
2022-08-04 18:52:13 +0300

    feat: greeting

---
+++ js/showgreeting.js
@@ -0,0 +1,26 @@
+const greetingtext = document.querySelector('.greeting');
+const name = document.querySelector('.name');
+
+function getTimeOfDay() {
+    let hour = new Date().getHours();
+    let timeOfDay = ''
+    if (hour >= 6 && hour < 12) timeOfDay = "Доброе утро"
+    if (hour >= 12 && hour < 18) timeOfDay = "Добрый день"
+    if (hour >= 18) timeOfDay = "Добрый вечер"
+    if (hour < 6) timeOfDay = "Доброй ночи"
+    greetingtext.textContent = timeOfDay;
+    setTimeout(getTimeOfDay, 1000);
+  }
+  getTimeOfDay();
+
+  function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+  }
+  window.addEventListener('beforeunload', setLocalStorage)
+
+  function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+  }
+  window.addEventListener('load', getLocalStorage)
