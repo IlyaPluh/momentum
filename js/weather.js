b5729510a74374ac275731a0499aad306963ce12
IlyaPluh <ilyaandriyanov@mail.ru>
2022-08-05 19:03:31 +0300

    feat: weather

---
+++ js/weather.js
@@ -0,0 +1,36 @@
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+window.addEventListener('load', getLocalStorage)
+
+async function getWeather() {
+try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;;
+    const res = await fetch(url);
+    const data = await res.json();
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${Math.round(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+} catch (err) {
+    alert("Город введен неверно")
+    city.value = 'Минск'
+    getWeather()
+}
+  }
+
+setTimeout(getWeather, 200)
+city.addEventListener('change', getWeather)
+
+function setLocalStorage() {
+    localStorage.setItem('city', city.value);
+  }
+  window.addEventListener('beforeunload', setLocalStorage)
+
+  function getLocalStorage() {
+    if(localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city');
+    }
+  }
