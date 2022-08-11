e375600b087fdf073422d2e9b2904808f65916a4
IlyaPluh <ilyaandriyanov@mail.ru>
2022-08-11 14:54:55 +0300

    feat: language

--- js/showtime.js
+++ js/showtime.js
@@ -1,10 +1,34 @@
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const options = {weekday: 'long', day: 'numeric', month: 'long', timeZone: 'UTC'};
+const language = document.querySelector('.selectlanguage');
 
-const showTime = () => {
-    time.textContent = new Date().toLocaleTimeString();
-    date.textContent = new Date().toLocaleDateString('ru-RU', options);
-    setTimeout(showTime, 1000);
+const Time = () => {
+  time.textContent = new Date().toLocaleTimeString();
+  setTimeout(Time, 1000);
+}
+Time()
+
+const showTime = (lang) => {
+    date.textContent = new Date().toLocaleDateString(lang, options);
   }
-  showTime();
+  showTime('ru-RU');
+  localStorage.getItem('language') === 'en-EN' ? showTime('en-EN') : showTime('ru-RU')
+
+  language.addEventListener('change', (event) => {
+    if (event.target.value === 'ru') {
+      showTime('ru-RU');
+    } if (event.target.value === 'en') {
+      showTime('en-EN');
+    }
+  })
+
+  function setLocalStorage() {
+    localStorage.setItem('language', language.value);
+  }
+  window.addEventListener('beforeunload', setLocalStorage)
+
+  function getLocalStorage() {
+      language.value = localStorage.getItem('language');
+  }
+  window.addEventListener('load', getLocalStorage)
--- js/weather.js
+++ js/weather.js
@@ -3,10 +3,11 @@ const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
 const city = document.querySelector('.city');
+const selectlanguage = document.querySelector('.selectlanguage');
 
-async function getWeather() {
+async function getWeather(lang) {
 try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=64ce87003c2eeed860b02b93f2accf60&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${lang}&appid=64ce87003c2eeed860b02b93f2accf60&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
     weatherIcon.className = 'weather-icon owf';
@@ -21,16 +22,29 @@ try {
   }
 
 //getWeather()
-setTimeout(getWeather, 400)
+setTimeout(getWeather, 400, 'ru')
+localStorage.getItem('selectlanguage') === 'en' ? setTimeout(getWeather, 400, 'en') : setTimeout(getWeather, 400, 'ru')
 
-city.addEventListener('change', getWeather)
+city.addEventListener('change', () => {
+  localStorage.getItem('selectlanguage') === 'en' ? getWeather('en') : getWeather('ru')
+})
+
+selectlanguage.addEventListener('change', (event) => {
+  if (event.target.value === 'ru') {
+    getWeather('ru')
+  } if (event.target.value === 'en') {
+    getWeather('en')
+  }
+})
 
 function setLocalStorage() {
     localStorage.setItem('city', city.value);
+    localStorage.setItem('selectlanguage', selectlanguage.value);
   }
 window.addEventListener('beforeunload', setLocalStorage)
 
   function getLocalStorage() {
+    selectlanguage.value = localStorage.getItem('selectlanguage');
     if(localStorage.getItem('city')) {
         city.value = localStorage.getItem('city');
     }
